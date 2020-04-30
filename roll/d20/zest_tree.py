4ddd934042a1bc15a68736a96a2fb9c64e46fdb7
Cole Brown <code@brown.dev>
2020-04-30 12:39:26 -0700

    finished tree.py unit tests

--- roll/d20/zest_tree.py
+++ roll/d20/zest_tree.py
@@ -25,6 +25,10 @@ from . import tree
 # -----------------------------------------------------------------------------
 
 
+# ------------------------------------Tree--------------------------------------
+# --                          Abstract / Base Class                           --
+# ------------------------------------------------------------------------------
+
 # -----------------------------------------------------------------------------
 # Test::Node (The OG Tree)
 # -----------------------------------------------------------------------------
@@ -211,6 +215,10 @@ class Test_Node(unittest.TestCase):
         self.assertEqual(expected, self.node0 ** self.node1)
 
 
+# ------------------------------------Tree--------------------------------------
+# --                            Leaf Node Classes                             --
+# ------------------------------------------------------------------------------
+
 # -----------------------------------------------------------------------------
 # Test::Leaf (of Node)
 # -----------------------------------------------------------------------------
@@ -289,25 +297,27 @@ class Test_Dice(unittest.TestCase):
 
         self.dice1 = tree.Dice(3, 6)
 
-    def assert_dice_values(self):
-        self.assertEqual(self.dice0.value, self.value0)
-        self.assertEqual(self.dice0,       self.dice0.value)
-
-        self.assertEqual(self.dice1.value, self.value1)
-        self.assertEqual(self.dice1,       self.dice1.value)
-
     def tearDown(self):
         self.dice0 = None
         self.dice1 = None
-
-        self.value0 = None
-        self.value1 = None
 
     # --------------------------------------------------------------------------
     # Roll 'em
     # --------------------------------------------------------------------------
 
-    def test_roll(self):
+    def test_roll_dice(self):
+        self.dice0._eval()
+        self.dice1.eval()
+
+        # They both rolled something and saved it to .roll?
+        self.assertTrue(self.dice0.roll)
+        self.assertTrue(self.dice1.roll)
+
+        # They both rolled the correct number of dice?
+        self.assertEqual(len(self.dice0.roll), self.dice0.dice)
+        self.assertEqual(len(self.dice1.roll), self.dice1.dice)
+
+    def test_roll_and_value(self):
         self.dice0._eval()
         self.dice1.eval()
 
@@ -319,17 +329,328 @@ class Test_Dice(unittest.TestCase):
         self.assertEqual(self.dice0.value, sum(self.dice0.roll))
         self.assertEqual(self.dice1.value, sum(self.dice1.roll))
 
-#         self.assertEqual(self.dice0._sign, 1)
-#         self.assertEqual(self.dice0._sign, self.dice1._sign)
-#
-#         self.dice0.neg()
-#         self.assertEqual(self.dice0._sign, -1)
-#         self.assertNotEqual(self.dice0._sign, self.dice1._sign)
-#
-#         # back around to the positive
-#         self.dice0.neg()
-#         self.assertEqual(self.dice0._sign, 1)
-#         self.assertEqual(self.dice0._sign, self.dice1._sign)
+
+# -----------------------------------------------------------------------------
+# Test::Constant (of Leaf of Node)
+# -----------------------------------------------------------------------------
+
+class Test_Constant(unittest.TestCase):
+
+    def setUp(self):
+        self.value0 = 42
+        self.const0 = tree.Constant(self.value0)
+        self.value1 = 9001
+        self.const1 = tree.Constant(self.value1)
+
+    def tearDown(self):
+        self.const0 = None
+        self.const1 = None
+
+        self.value0 = None
+        self.value1 = None
+
+    # --------------------------------------------------------------------------
+    # Test that nothing happens in a specific way...
+    # --------------------------------------------------------------------------
+
+    def test_roll_const(self):
+        # Constants should already have their value assigned after creation, as
+        # they are... constant.
+        self.assertEqual(self.value0, self.const0.value)
+        self.assertEqual(self.value1, self.const1.value)
+        self.assertEqual(self.value0, self.const0)
+        self.assertEqual(self.value1, self.const1)
+
+        self.const0._eval()
+        self.const1.eval()
+
+        # Nothing should have changed after evaluation, as they are... constant.
+        self.assertEqual(self.value0, self.const0.value)
+        self.assertEqual(self.value1, self.const1.value)
+        self.assertEqual(self.value0, self.const0)
+        self.assertEqual(self.value1, self.const1)
+
+
+# -----------------------------------------------------------------------------
+# Test::Variable (of Leaf of Node)
+# -----------------------------------------------------------------------------
+
+class Test_Variable(unittest.TestCase):
+
+    def setUp(self):
+        self.name0 = "$jeff-mod"
+        self.value0 = 3
+        self.var0 = tree.Variable(self.name0)
+        self.name1 = "$jeff"
+        self.value1 = 1336
+        self.var1 = tree.Variable(self.name1)
+
+    def tearDown(self):
+        self.var0 = None
+        self.var1 = None
+
+        self.name0 = None
+        self.name1 = None
+
+        self.value0 = None
+        self.value1 = None
+
+    # --------------------------------------------------------------------------
+    # Test... the placeholder holds its place?
+    # --------------------------------------------------------------------------
+
+    def test_roll_var(self):
+        # Variables should just be a name until we know what to do with
+        # that name...
+        # So they shouldn't have a value just yet.
+        self.assertEqual(None, self.var0.value)
+        self.assertEqual(None, self.var1.value)
+        self.assertEqual(None, self.var0)
+        self.assertEqual(None, self.var1)
+        self.assertNotEqual(self.value0, self.var0.value)
+        self.assertNotEqual(self.value1, self.var1.value)
+        self.assertNotEqual(self.value0, self.var0)
+        self.assertNotEqual(self.value1, self.var1)
+
+        # But they should have their name.
+        self.assertEqual(self.name0, self.var0.name)
+        self.assertEqual(self.name1, self.var1.name)
+
+        self.var0._eval()
+        self.var1.eval()
+
+        # TODO: They should have their values - the actual values. But we
+        # haven't implemented that yet and only set to 0 on eval.
+        self.assertEqual(0, self.var0.value)
+        self.assertEqual(0, self.var1.value)
+        self.assertEqual(0, self.var0)
+        self.assertEqual(0, self.var1)
+        self.assertNotEqual(self.value0, self.var0.value)
+        self.assertNotEqual(self.value1, self.var1.value)
+        self.assertNotEqual(self.value0, self.var0)
+        self.assertNotEqual(self.value1, self.var1)
+
+
+# ------------------------------------Tree--------------------------------------
+# --                             Branch Classes                               --
+# ------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Test::Branch (of Node)
+# -----------------------------------------------------------------------------
+
+class Test_Branch(unittest.TestCase):
+
+    def test_eval(self):
+        with self.assertRaises(AttributeError) as context:
+            # Branch base class shouldn't be able to eval successfully...
+            branch = tree.Branch(None)
+            branch.eval()
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorMath (of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorMath(unittest.TestCase):
+
+    def test_eval(self):
+        with self.assertRaises(TypeError) as context:
+            # OperatorMath base class shouldn't be able to eval successfully...
+            branch = tree.OperatorMath(None, None, None)
+            branch.eval()
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorAdd (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorAdd(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.add = tree.OperatorAdd(self.children)
+
+    def tearDown(self):
+        self.add = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_add(self):
+        self.assertEqual(0, self.add.value)
+        self.assertEqual(0, self.add)
+
+        self.add.eval()
+
+        expected = self.value0 + self.value1
+        self.assertEqual(expected, self.add.value)
+        self.assertEqual(expected, self.add)
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorSub (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorSub(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.sub = tree.OperatorSub(self.children)
+
+    def tearDown(self):
+        self.sub = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_sub(self):
+        self.assertEqual(0, self.sub.value)
+        self.assertEqual(0, self.sub)
+
+        self.sub.eval()
+
+        expected = self.value0 - self.value1
+        self.assertEqual(expected, self.sub.value)
+        self.assertEqual(expected, self.sub)
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorMult (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorMult(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.mult = tree.OperatorMult(self.children)
+
+    def tearDown(self):
+        self.mult = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_mult(self):
+        self.assertEqual(0, self.mult.value)
+        self.assertEqual(0, self.mult)
+
+        self.mult.eval()
+
+        expected = self.value0 * self.value1
+        self.assertEqual(expected, self.mult.value)
+        self.assertEqual(expected, self.mult)
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorDiv (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorDiv(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.div = tree.OperatorDiv(self.children)
+
+    def tearDown(self):
+        self.div = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_div(self):
+        self.assertEqual(0, self.div.value)
+        self.assertEqual(0, self.div)
+
+        self.div.eval()
+
+        expected = self.value0 / self.value1
+        self.assertEqual(expected, self.div.value)
+        self.assertEqual(expected, self.div)
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorMod (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorMod(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.mod = tree.OperatorMod(self.children)
+
+    def tearDown(self):
+        self.mod = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_mod(self):
+        self.assertEqual(0, self.mod.value)
+        self.assertEqual(0, self.mod)
+
+        self.mod.eval()
+
+        expected = self.value0 % self.value1
+        self.assertEqual(expected, self.mod.value)
+        self.assertEqual(expected, self.mod)
+
+
+# -----------------------------------------------------------------------------
+# Test::OperatorPow (of OperatorMath of Branch of Node)
+# -----------------------------------------------------------------------------
+
+class Test_OperatorPow(unittest.TestCase):
+    def setUp(self):
+        self.value0 = 42
+        self.value1 = 9001
+        self.children = [
+            tree.Constant(self.value0),
+            tree.Constant(self.value1),
+        ]
+        self.pow = tree.OperatorPow(self.children)
+
+    def tearDown(self):
+        self.pow = None
+        self.children = None
+
+        self.value0 = None
+        self.value1 = None
+
+    def test_pow(self):
+        self.assertEqual(0, self.pow.value)
+        self.assertEqual(0, self.pow)
+
+        self.pow.eval()
+
+        expected = self.value0 ** self.value1
+        self.assertEqual(expected, self.pow.value)
+        self.assertEqual(expected, self.pow)
 
 
 # --------------------------------Unit Testing----------------------------------
