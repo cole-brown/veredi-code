f35315382117e295055f94091fef4f29d3a19f55
Cole Brown <code@brown.dev>
2020-05-25 20:46:48 -0700

    FileTreeRepository unit tests.
    Updated zest_data/zest.py to use pathlib.
    
    Updated zest_file to test loading of each current load type.

--- data/repository/zest_file.py
+++ data/repository/zest_file.py
@@ -40,66 +40,65 @@ class Test_FileTreeRepo(unittest.TestCase):
 
     def context_load(self, type):
         ctx = DataLoadContext('unit-testing', type, 'test-campaign')
+        path = self.path / 'test-campaign'
         if type == DataContext.Type.PLAYER:
             ctx.sub['user'] = 'u/jeff'
             ctx.sub['player'] = 'Sir Jeffsmith'
+            # hand-craft the expected escaped/safed path
+            path = path / 'players' / 'u_jeff' / 'sir_jeffsmith.yaml'
         elif type == DataContext.Type.MONSTER:
-            ctx.sub = ctx.subcontext
             ctx.sub['family'] = 'dragon'
             ctx.sub['monster'] = 'aluminum dragon'
+            # hand-craft the expected escaped/safed path
+            path = path / 'monsters' / 'dragon' / 'aluminum_dragon.yaml'
         elif type == DataContext.Type.NPC:
-            ctx.sub = ctx.subcontext
             ctx.sub['family'] = 'Townville'
-            ctx.sub['player'] = 'Sword Merchant'
+            ctx.sub['npc'] = 'Sword Merchant'
+            # hand-craft the expected escaped/safed path
+            path = path / 'npcs' / 'townville' / 'sword_merchant.yaml'
         elif type == DataContext.Type.ITEM:
-            ctx.sub = ctx.subcontext
             ctx.sub['category'] = 'weapon'
             ctx.sub['item'] = 'Sword, Ok'
+            # hand-craft the expected escaped/safed path
+            path = path / 'items' / 'weapon' / 'sword__ok.yaml'
         else:
             raise exceptions.LoadError(
                 f"No DataContext.Type to ID conversion for: {type}",
                 None,
                 self.context.merge(context))
 
-        return ctx
+        return ctx, path
 
     def test_init(self):
         self.assertTrue(self.repo)
         self.assertTrue(self.path)
         self.assertTrue(os.path.isdir(self.path))
 
-    def test_load(self):
-        context = self.context_load(DataContext.Type.PLAYER)
+    def do_load_test(self, load_type):
+        context, path = self.context_load(load_type)
+        self.assertTrue(path.parent.exists())
 
-        loaded = self.repo.load(context)
-        self.assertIsNotNone(loaded)
+        loaded_stream = self.repo.load(context)
+        self.assertIsNotNone(loaded_stream)
 
-        # TODO: read file directly, assert contents are same.
+        # read file directly, assert contents are same.
+        with path.open(mode='r') as file_stream:
+            self.assertIsNotNone(file_stream)
 
+            file_data = file_stream.read(None)
+            repo_data = loaded_stream.read(None)
+            self.assertIsNotNone(file_data)
+            self.assertIsNotNone(repo_data)
+            self.assertEqual(repo_data, file_data)
 
-#     def test_metadata(self):
-#         loaded = None
-#         with open(self.path, 'r') as f:
-#             loaded = self.codec._load_all(f, self.context())
-#
-#         self.assertIsNotNone(loaded)
-#         self.assertEqual(type(loaded[0]), DocMetadata)
-#         metadata = loaded[0].decode()
-#
-#         self.assertEqual(metadata['doc-type'],
-#                          'veredi.unit-test')
-#         self.assertEqual(metadata['version'],
-#                          datetime.date(2020, 5, 19))
-#         self.assertEqual(metadata['source'],
-#                          'veredi.unit-test')
-#         self.assertEqual(metadata['author'],
-#                          'Cole Brown')
-#         self.assertEqual(metadata['date'],
-#                          datetime.date(2020, 5, 22))
-#         self.assertEqual(metadata['system'],
-#                          'unit-test')
-#         self.assertEqual(metadata['name'],
-#                          'veredi.unit-test.component.health')
-#         self.assertEqual(metadata['display-name'],
-#                          'Veredi Unit-Testing Health Component')
-#
+    def test_load_player(self):
+        self.do_load_test(DataContext.Type.PLAYER)
+
+    def test_load_monster(self):
+        self.do_load_test(DataContext.Type.MONSTER)
+
+    def test_load_npc(self):
+        self.do_load_test(DataContext.Type.NPC)
+
+    def test_load_item(self):
+        self.do_load_test(DataContext.Type.ITEM)
