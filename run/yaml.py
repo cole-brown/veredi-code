023b1e2095a7a3559dbf7fb3177912db5ce91301
Cole Brown <code@brown.dev>
2021-02-28 13:27:26 -0800

    v://000f Split up formats/yaml/yaml.py
    Split yaml.py up into:
      - logs/log/formats/yaml.py
        - Will be renamed next commit.
        - LogYaml, our YAML logging.Formatter.
      - logs/log/formats/factory.py
        - LogRecordFactory: not yet used
      - logs/log/formats/record.py
        - LogRecordYaml: Veredi's YAML record outputter
      - logs/log/formats/__init__.py
        - empty for now
      - run/yaml.py
        - The representers for FoldedString, LiteralString, OrderedDict.
    
    That's it. Just cut/pasted blocks.
    
    v://000f/2021-02-23T10:46:52-0800/bug/logging-layout

---
+++ run/yaml.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+
+'''
+Custom YAML Formatter for:
+  - Logging
+  - Serialization/Deserialiazition (Serdes).
+'''
+
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+
+from typing import Any, Mapping
+
+
+import yaml
+from collections import OrderedDict
+
+
+# Logging needs this stuff for YAML formatted logs, so do not log from here.
+# from veredi.logs               import log
+
+from veredi.base.strings       import label
+from veredi.data.config.config import Configuration
+from veredi.data.exceptions    import ConfigError
+
+
+# -----------------------------------------------------------------------------
+# Constants
+# -----------------------------------------------------------------------------
+
+_DOTTED = "veredi.run.yaml"
+
+_REPRESENTERS = set()
+
+
+# -----------------------------------------------------------------------------
+# YAML Large String Dumpers
+# -----------------------------------------------------------------------------
+
+class FoldedString(str):
+    '''
+    Class just marks this string to be dumped in 'folded' YAML string format.
+    '''
+    pass
+
+
+class LiteralString(str):
+    '''
+    Class just marks this string to be dumped in 'literal' YAML string format.
+    '''
+    pass
+
+
+def folded_string_representer(dumper: yaml.Dumper,
+                              data:   str) -> yaml.ScalarNode:
+    '''
+    Register FoldedString as the correct style of literal.
+    '''
+    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='>')
+
+
+def literal_string_representer(dumper: yaml.Dumper,
+                               data:   str) -> yaml.ScalarNode:
+    '''
+    Register FoldedString as the correct style of literal.
+    '''
+    return dumper.represent_scalar(u'tag:yaml.org,2002:str', data, style='|')
+
+
+def ordered_dict_representer(dumper: yaml.Dumper,
+                             data:   Mapping) -> yaml.MappingNode:
+    '''
+    Register OrderedDict in order to be able to dump it.
+    '''
+    return dumper.represent_mapping(u'tag:yaml.org,2002:map',
+                                    data.items(),
+                                    flow_style=False)  # block flow style
+
+
+# -----------------------------------------------------------------------------
+# Register representers with YAML.
+# -----------------------------------------------------------------------------
+
+def representers() -> None:
+    '''
+    Reentrant: Add additional representers needed to YAML.
+    '''
+    yaml.add_representer(FoldedString,
+                         folded_string_representer,
+                         Dumper=yaml.SafeDumper)
+    yaml.add_representer(LiteralString,
+                         literal_string_representer,
+                         Dumper=yaml.SafeDumper)
+    yaml.add_representer(OrderedDict,
+                         ordered_dict_representer,
+                         Dumper=yaml.SafeDumper)
+
+
+# -----------------------------------------------------------------------------
+# Automatically set up when imported.
+# -----------------------------------------------------------------------------
+
+representers()
