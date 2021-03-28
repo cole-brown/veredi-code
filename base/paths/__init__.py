8e7c504b92d13ff1b4927944961b8076b7155b32
Cole Brown <code@brown.dev>
2021-03-28 13:17:40 -0700

    Cleaned up run.regitry.
    Performance is acceptable for now.
    
    Cleaned up run/registry.py from perf testing work.
      - Updated ignore dirs to work like ignore files - strs and/or regexes.
      - Updated ignore dirs to work like ignore files - get from config or
        fallback to defaults.
      - Updated config path accessors to work for new location of file
        ignores and added dir ignores.
    
    Updated config to be able to store ignores for both files and dirs.
    
    Fixed `force_test` to return proper keys for the force testing flag in
    the config data.
    
    Now passing ignore_dirs into `_find_modules()`.
    
    Deleted all the old `_find_*()` functions. Renamed `_find_scandir2()` to
    `_scan_tree()`.
    
    Deleted commented out code block and some commented out prints.
    
    Updated `_ignore_dir()` to work like `_ignore()` - strs and/or regexes.
      - Updated its logging too.
    
    Updated zprofile_registry.py to run test successfully.
    
    Updated all testing config files to have new ignore structure.

--- base/paths/__init__.py
+++ base/paths/__init__.py
@@ -22,7 +22,7 @@ from .const  import PathType, PathTypeTuple, PathsInput
 # Functions
 # ------------------------------
 
-from .utils  import (cast, walk_filtered, exists,
+from .utils  import (cast, exists,
                      is_file, is_dir,
                      to_str_list, to_str,
                      serialize_claim, serialize)
@@ -58,7 +58,6 @@ __all__ = [
     # Functions
     # ------------------------------
     'cast',
-    'walk_filtered',
     'exists',
     'is_file',
     'is_dir',
