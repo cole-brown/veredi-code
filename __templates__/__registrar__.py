db0055ad172872018614e0a01e9722bee3128a3f
Cole Brown <code@brown.dev>
2021-04-01 12:57:50 -0700

    v://0012 Updated templates.
    Updated "__register__.py" to be a more up-to-date and complete example
    of registration.
    
    Deleted encodable-specific template - the normal template includes both
    our current registrars so it's a good example for multiple now.
    
    Updated "__registrar__.py" to reference the real one, since I don't
    expect to have many of those and currently intend to have just the one.

--- __templates__/__registrar__.py
+++ __templates__/__registrar__.py
@@ -1,36 +1,7 @@
-# coding: utf-8
-
-'''
-Module for auto-magical registration shenanigans.
-
-This will be found and imported by run.registry in order to have whatever
-Registries and Registrars this provides available at run-time.
-'''
-
-# -----------------------------------------------------------------------------
-# Imports
-# -----------------------------------------------------------------------------
-
-from veredi.logs.log import Group
-
-
-# ------------------------------
-# Registries & Registrars
-# ------------------------------
-# from .registry import registrar
-
-
-# -----------------------------------------------------------------------------
-# Registration
-# -----------------------------------------------------------------------------
-
-# registrar([Group.START_UP, Group.REGISTRATION], None)
-
-
-# -----------------------------------------------------------------------------
-# Exports
-# -----------------------------------------------------------------------------
-
-__all__ = [
-    # No exports? Just a registration thing.
-]
+# For Veredi code:
+#   Put your registrar in with the rest at:
+#     - veredi.data.registration.__registrar__
+#
+# For others:
+#   See what `veredi.data.registration.__registrar__` is doing and maybe do
+#   something similar.
